b8aeda7aca45a6cebad23bea014aea7e2dec796e
Riddhi Mistry <32605242+mistryrm@users.noreply.github.com>
2023-10-12 20:06:46 -0400

    Fixed script typo

--- static/js/app.js
+++ static/js/app.js
@@ -138,7 +138,7 @@ function generateMetadata(data) {
 }
 
 function onOptionChange(value) {
-    d3.json("samples.json").then(function (data) {
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function (data) {
 
         const sample = data.samples.find(data => data.id === value);
         if (sample) {
